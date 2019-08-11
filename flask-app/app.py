f931caef50285c1dcfa96e0c8d9c3c58a7958bee
Claus Freilev Krag <cfk@cardlay.com>
2019-08-11 14:50:44 +0200

    added files

---
+++ flask-app/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import json
+
+from flask import Flask
+from flask import abort
+from flask import make_response
+from flask import render_template
+from flask import request
+from flask_basicauth import BasicAuth
+
+from model.dms import DMS as DMS_APPLICATION
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def say_hello() -> str:
+    return "hello to you!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
